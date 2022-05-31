03ef656b001bde6c1cc36b55130633cfb7c2ea51
Ziad Khattab <ziadkh@amazon.com>
2022-05-30 20:29:47 -0400

    Penis

--- types/types.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="https://esm.sh/@types/snowpack-env@2.3.4/index.d.ts" />
-/// <reference types="https://esm.sh/@cloudflare/workers-types@3.11.0/index.d.ts" />
-/// <reference types="./static.d.ts" />
