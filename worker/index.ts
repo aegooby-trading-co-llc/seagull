8a5c043cd0d31079ea5a591bded876d53ccaf368
aegooby <aegooby@gmail.com>
2022-06-10 10:27:33 -0400

    Penis.

--- worker/index.ts
+++ worker/index.ts
@@ -2,21 +2,19 @@ import * as mime from "mime";
 import * as ReactDOMServer from 'react-dom/server';
 import { element } from "./index.html.jsx";
 
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
     // https://developers.cloudflare.com/workers/runtime-apis/kv/
     STATIC_CONTENT: KVNamespace;
 }
 
+/**
+ * Allows for request URL paths to contain the file hash or not.
+ * For example, transforms /app/index@IKXSF6WX.js to /app/index.js,
+ * but /app/index.js is left alone.
+ * 
+ * @param path URL path to have its hash removed
+ * @returns URL path without the hash
+ */
 function stripHash(path: string): string {
     const hashRegex = /(.*)@([A-Z0-9]{8})(\..*)/;
     if (hashRegex.test(path)) {
@@ -60,6 +58,7 @@ export default {
     async fetch(
         request: Request,
         env: Env,
+        // eslint-disable-next-line
         ctx: ExecutionContext
     ): Promise<Response> {
         return await route(request, env);
