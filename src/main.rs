e86a363289cf749de8cfdb6cba5f0ffd97db5c20
tostig77 <venika@bu.edu>
2022-06-17 14:21:51 -0400

    Penis

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
-fn main() {
-    let re = regex::Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-    println!("Did our penis match? {}", re.is_match("2014-01-01"));
+async fn hello_world(
+    _req: hyper::Request<hyper::Body>,
+) -> Result<hyper::Response<hyper::Body>, std::convert::Infallible> {
+    Ok(hyper::Response::new("Hello, World".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    // We'll bind to 127.0.0.1:3000
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = hyper::service::make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, std::convert::Infallible>(hyper::service::service_fn(hello_world))
+    });
+
+    let server = hyper::Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
