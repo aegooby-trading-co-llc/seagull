a75215aaf3906b12614218fa7cff18804967b76c
tostig77 <venika@bu.edu>
2022-06-17 16:29:26 -0400

    Penisasaurus rex.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,49 @@
-async fn hello_world(
-    _req: hyper::Request<hyper::Body>,
+mod schema;
+
+#[derive(Clone)]
+struct Context {
+    // Whatever data your application needs can go here
+}
+
+async fn handle(
+    _context: Context,
+    _addr: std::net::SocketAddr,
+    req: hyper::Request<hyper::Body>,
 ) -> Result<hyper::Response<hyper::Body>, std::convert::Infallible> {
-    Ok(hyper::Response::new("Hello, World".into()))
+    let mut response = hyper::Response::new("Penile, World".into());
+    let root_node = std::sync::Arc::new(juniper::RootNode::new(
+        schema::Query,
+        juniper::EmptyMutation::<()>::default(),
+        juniper::EmptySubscription::<()>::default(),
+    ));
+    match (req.method(), req.uri().path()) {
+        (&hyper::Method::GET, "/graphql") => {
+            response = juniper_hyper::graphiql("/graphql", None).await;
+        }
+        (&hyper::Method::POST, "/graphql") => {
+            response = juniper_hyper::graphql(root_node, std::sync::Arc::new(()), req).await;
+        }
+        (&hyper::Method::POST, "/auth") => {
+            // authentication (gay)
+        }
+        (&hyper::Method::GET, _) => {
+            // pages and stuff
+        }
+        _ => {
+            // what the fuck did you do
+            // error
+            // retard
+            *response.status_mut() = hyper::StatusCode::NOT_FOUND;
+        }
+    };
+    return Ok(response);
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
 }
 
 #[tokio::main]
@@ -9,17 +51,31 @@ async fn main() {
     // We'll bind to 127.0.0.1:3000
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
-    let make_svc = hyper::service::make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, std::convert::Infallible>(hyper::service::service_fn(hello_world))
-    });
+    let context = Context {};
+    let make_service =
+        hyper::service::make_service_fn(move |conn: &hyper::server::conn::AddrStream| {
+            // We have to clone the context to share it with each invocation of
+            // `make_service`. If your data doesn't implement `Clone` consider using
+            // an `std::sync::Arc`.
+            let context = context.clone();
 
-    let server = hyper::Server::bind(&addr).serve(make_svc);
+            // You can grab the address of the incoming connection like so.
+            let addr = conn.remote_addr();
+
+            // Create a `Service` for responding to the request.
+            let service = hyper::service::service_fn(move |req| handle(context.clone(), addr, req));
+
+            // Return the service to hyper.
+            async move { Ok::<_, std::convert::Infallible>(service) }
+        });
+
+    let server = hyper::Server::bind(&addr).serve(make_service);
+
+    // And now add a graceful shutdown signal...
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
 
     // Run this server for... forever!
-    if let Err(e) = server.await {
+    if let Err(e) = graceful.await {
         eprintln!("server error: {}", e);
     }
 }
